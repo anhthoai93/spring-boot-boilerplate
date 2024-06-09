ca9c27a4375adf26ecb5b70769057c9f601ec056
Andy <thoaitran93@gmail.com>
2024-06-09 11:38:50 +0700

    cicd

---
+++ src/main/java/com/andy/springboot/boilerplate/GreetingController.java
@@ -0,0 +1,15 @@
+package com.andy.springboot.boilerplate;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+    @GetMapping
+    public ResponseEntity<String> greeting() {
+        return ResponseEntity.ok().body("Hello, How are you?");
+    }
+}
